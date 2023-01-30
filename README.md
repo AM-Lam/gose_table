1e434f3ab229716fb7a20f529c7137da7a82ae76
AM-Lam <50862915+AM-Lam@users.noreply.github.com>
2023-01-30 20:33:25 +1100

    Add files via upload
    basic combine the database with some mistake, how to deal with description instead of combining the next row of data

---
+++ README.md
@@ -0,0 +1,14 @@
+# Going Seventeen episode list based on wiki
+Generate a list of going seventeen episodes from 2020 to 2022, with the information of year and number of episodes. 
+
+## How to make 
+Fetch the data tables from wikipedia page [here](https://en.wikipedia.org/wiki/Going_Seventeen_(web_series)#Episodes). 
+Combine the provided tables from different years(2020, 2021, 2022) by using the library pandas or sql(TODO: hasn't decided yet).
+Modify the result database for my research uses and output it to a csv file. 
+
+
+## development 
+The project will produce a csv file, called gose.csv, with the updated information. 
+1. run `python3 gose.py` and pray that it will work
+
+It is meant for integrated with notions or my own web pages. 
---
+++ gose_wiki.py
@@ -0,0 +1,111 @@
+# import requests
+import urllib.request
+import bs4 as bs
+# import time
+# import numpy as np
+import pandas as pd
+# from urllib.request import urlopen
+
+
+
+# fetch wiki page from beautifulSoup
+url = 'https://en.wikipedia.org/wiki/Going_Seventeen_(web_series)'
+source = urllib.request.urlopen(url).read()
+soup = bs.BeautifulSoup(source,'html.parser')
+
+# get all the tables from the page
+tables = soup.find_all('table', {'class':'wikitable'})
+
+# get the values of title holder:
+tables = tables[1:6]
+# print(tables)
+
+# for url in soup.find_all('table'):
+#     print(url.get('href'))
+
+
+# Now when you closely observe the above HTML code, you understand that the row tags are given with <tr> and the columns tags are with<td>. We retrieve all the rows and columns using the find_all() method. For example table.find_all(‘tr’) this collects all the rows from the table.
+
+# We create a list named movie_list to store all the names.
+# We iterate through the entire table from 2nd row to collect the names.
+
+num_tables = 1
+df = pd.DataFrame([])
+df_list = []
+df_headers = []
+
+for table in tables:
+    pd_to_tables = []
+    
+    table = table.find('tbody')
+    
+    if (num_tables == 1):
+        for t in table.find_all('tr')[0]:
+            df_headers.append(t.text)
+        df_headers.append('Descriptions')
+    
+    # little flag for combining every 2 rows
+    isAdd = False
+    row = []
+    check_row_num = -1
+    for tr in table.find_all('tr')[1:]:
+        # for e in tr:
+        #     row.append(e.text)
+        check_row_num += 1
+        if isAdd:
+            row.append(tr.text)
+            pd_to_tables.append(row)
+
+            row = []
+            isAdd = False
+            continue
+        else:  
+            for e in tr:
+                row.append(e.text)
+                
+            isAdd = True
+
+    pd_to_tables = pd.DataFrame(pd_to_tables)
+    pd_to_tables.columns = df_headers
+    df = pd_to_tables.copy()
+    
+    if (num_tables == 1):
+        df_list = df.copy()
+    else:
+        df_list = pd.concat([df_list, df])
+    
+    # df_list = pd.concat([df_list, df]) if num_tables == 1 else df.copy()
+    
+    num_tables += 1
+
+df_list.to_csv('temp.csv', encoding='utf-8', index=False)
+# # add to pd
+# # print(table_title)
+# # print(pd_to_tables[1][1])
+# # print(df)
+# # result = pd.concat(df_list)
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#     print (df_list)
+# # print(df_list)
+# # df_list.head(3)
+
+# # Finally, we’re able to solve this by collecting the title attribute and get the required output.
+
+# # We collected all the rows except the 1st row from the req_table using find_all(‘tr’)[1:].
+# # Then collected all the column data for each row using tr.find_all(‘td’).
+# # The movie title is ialln the anchor tag with the name ‘title’. As there is only one title for each <a> we using find() method to grab the title and append it to the list.
+# mylist = []
+# for tr in tables.find_al numpy as np
+# import pandas as pdl('tr')[1:]:
+#     tds = tr.find_all('td')
+#     name = tds[1].contents# Get the attribute of all the tables from the first tables
+    # for tr_t in tr:
+    #     table_title.append(tr_t.text)
+#     ##print(tds) 
+#     ##print(name)
+#     ##print("---")
+#     a = name.find('a') 
+#     mylist.append(a.get('title'))
+# print(my_list)
+
+
